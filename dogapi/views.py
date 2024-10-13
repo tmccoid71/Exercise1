2ef1a711ddf9d01dc798b63ab3616a1683c27b20
Tyler M <tmccoid@unomaha.edu>
2024-10-12 21:03:02 -0500

    Removed Excess Code

--- dogapi/views.py
+++ dogapi/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Dog, Breed
 from .serializers import DogSerializer, BreedSerializer
+
+class BreedList(generics.ListCreateAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
 
 class DogList(generics.ListCreateAPIView):
     queryset = Dog.objects.all()
@@ -11,10 +18,3 @@ class DogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
-class BreedList(generics.ListCreateAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
-
-class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
