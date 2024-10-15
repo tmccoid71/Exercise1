20fccc04b7a01c3356201abb798e99edc86c1dad
Tyler M <tmccoid@unomaha.edu>
2024-10-14 20:09:49 -0500

    Changed to Viewsets

--- Exercise1/urls.py
+++ Exercise1/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from dogapi.views import DogList, DogDetail, BreedList, BreedDetail
+from django.urls import path, include
+from rest_framework import routers
+from dogapi.views import DogViewSet, BreedViewSet
 
+router = routers.SimpleRouter()
+router.register(r'api/dogs', DogViewSet)
+router.register(r'api/breeds', BreedViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/dogs/', DogList.as_view(), name='dog-list'),
-    path('api/dogs/<int:pk>/', DogDetail.as_view(), name='dog-detail'),
-    path('api/breeds/', BreedList.as_view(), name='breed-list'),
-    path('api/breeds/<int:pk>/', BreedDetail.as_view(), name='breed-detail'),
+    path('', include(router.urls)),
 ]
--- dogapi/views.py
+++ dogapi/views.py
@@ -1,20 +1,14 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Dog, Breed
 from .serializers import DogSerializer, BreedSerializer
 
-class BreedList(generics.ListCreateAPIView):
+class BreedViewSet(viewsets.ModelViewSet):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
 
-class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
-
-class DogList(generics.ListCreateAPIView):
+class DogViewSet(viewsets.ModelViewSet):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
-class DogDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+
 
