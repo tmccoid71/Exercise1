5aac221715c692efd04d27932018e181e996dea7
Tyler M <tmccoid@unomaha.edu>
2024-10-24 20:58:04 -0500

    Adding Docker

--- Exercise1/settings.py
+++ Exercise1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Exercise1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',  # or the actual DB host
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
 
